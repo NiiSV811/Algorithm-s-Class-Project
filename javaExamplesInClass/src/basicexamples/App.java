b5bf3dd68f12767b85fb03f7f99d26643c7e6eb0
NiiSV811 <sarmiento.vargasnicolas.8@gmail.com>
2022-10-18 22:14:26 -0500

    Person Class

--- javaExamplesInClass/src/basicexamples/App.java
+++ javaExamplesInClass/src/basicexamples/App.java
@@ -1,38 +1,95 @@
-package greeting;
+package personclass;
 
 import javax.swing.*;
-
+import java.util.Date;
+import java.text.SimpleDateFormat;
 public class App {
     public static void main(String[] args) {
-        //Auxiliar Variables
+        //Auxiliary Variables
+
         String auxAge = "";
         String auxGenre = "";
+        String auxHeight = "";
+        String auxWeight = "";
+        String auxDate = "";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String auxMarried = "";
 
         // Variables to Set
         int age = 0;
         String name = "";
         boolean genre = false;
+        String id = "";
+        String rh = "";
+        float height = 0;
+        float weight = 0;
+        Date bornDate = null;
+        boolean married = false;
+        String skinColor = "";
+
 
         Person lulu = new Person();
 
         //Input
-        name = JOptionPane.showInputDialog("Ingresa nombre: ");
-        auxAge = JOptionPane.showInputDialog("Ingresa Edad : ");
-        auxGenre = JOptionPane.showInputDialog("Ingresa Género: \n true: M \nfalse: F ");
+        name = JOptionPane.showInputDialog(null, "Ingresa nombre: ", "Jonh Doe");
+        auxAge = JOptionPane.showInputDialog(null, "Ingresa Edad : ", "18");
+        auxGenre = JOptionPane.showInputDialog(null, "Ingresa Género: \n true: M \nfalse: F ", "false");
+        id = JOptionPane.showInputDialog(null, "Ingresa id : ", "T.I 1054262632");
+        rh = JOptionPane.showInputDialog(null,"Ingresa RH : ", "O+");
+        auxHeight = JOptionPane.showInputDialog(null, "Ingresa altura (metros): ", "1.59");
+        auxWeight = JOptionPane.showInputDialog(null, "Ingresa peso (kilogramos)", "60");
+        auxDate = JOptionPane.showInputDialog(null,"Ingresa edad de nacimiento: ", "01/01/2001");
+        auxMarried = JOptionPane.showInputDialog(null,"Ingrese estado casado: \n Casado: true \n Soltero: false","false");
+        skinColor = JOptionPane.showInputDialog(null, "Ingresa tonalidad de piel: ","moren@");
+
 
         //Casting
-        age = Integer.parseInt(auxAge);
-        genre = Boolean.parseBoolean(auxGenre);
+        try {
+            age = Integer.parseInt(auxAge);
+            genre = Boolean.parseBoolean(auxGenre);
+            height = Float.parseFloat(auxHeight);
+            weight = Float.parseFloat(auxWeight);
+            bornDate = dateFormat.parse(auxDate);
+            married = Boolean.parseBoolean(auxMarried);
+
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Entrada no valida X(... Vuelve a intentarlo");
+            System.exit(0);
+        }
 
         //Setting
+        lulu.setAge(age);
+        lulu.setName(name);
+        lulu.setGenre(genre);
+        lulu.setId(id);
+        lulu.setRh(rh);
+        lulu.setHeight(height);
+        lulu.setWeigth(weight);
+        lulu.setBornDate(bornDate);
+        lulu.setMarried(married);
+        lulu.setSkinColor(skinColor);
+
+        //Showing person attributes
+        JOptionPane.showMessageDialog(null,"Los atributos de "
+                + lulu.getName() + " son: " +
+                "\n Nombre: " + lulu.getName() +
+                "\n Id: " + lulu.getId()+
+                "\n Edad: " + lulu.getAge() + " años" +
+                "\n Genero: " + lulu.getGenre() + "        false: F  true: M" +
+                "\n RH: " + lulu.getRh()+
+                "\n Estatura: " + lulu.getHeight() + "    metros" +
+                "\n Peso: " + lulu.getWeight() + "     Kilogramos " +
+                "\n Fecha Nacimiento : " + dateFormat.format(lulu.getBornDate()) +
+                "\n Casad@? : " + lulu.getMarried() +
+                "\n Color de tez : " + lulu.getSkinColor()
+        );
+
+
 
 
 
 
     }
 
-
-
-
-    }
 }
+
--- javaExamplesInClass/src/basicexamples/Person.java
+++ javaExamplesInClass/src/basicexamples/Person.java
@@ -1,4 +1,6 @@
-package greeting;
+package personclass;
+
+import java.util.Date;
 
 public class Person {
 
@@ -6,7 +8,14 @@ public class Person {
     private int age;
     private boolean genre;
     private  String name;
-    //id(str), rh, height, weight, bornDate(Date) , married(boolean) , skinColor
+    private String id;
+    private String rh;
+    private float height;
+    private float weight;
+    private Date bornDate;
+    private boolean married;
+    private String skinColor;
+    
 
 
     //Constructor
@@ -14,10 +23,18 @@ public class Person {
         this.age = 0;
         this.genre = false;
         this.name = "";
+        this.id = "";
+        this.rh = "";
+        this.height = 0;
+        this.weight = 0;
+        this.bornDate = new Date();
+        this.married = false;
+        this.skinColor = "";
     }
 
     //Methods
         //Setting Methods
+        
     public void setAge(int age){
         this.age = age;
     }
@@ -28,19 +45,74 @@ public class Person {
         this.name = name;
     }
 
+    public void setId( String id){
+        this.id = id;
+    }
+
+    public void setRh( String rh){
+        this.rh = rh;
+    }
+
+    public void setHeight( float height){
+        this.height = height;
+    }
+
+    public void setWeigth( float weight){
+        this.weight = weight;
+    }
+
+    public void setBornDate( Date bornDate){
+        this.bornDate = bornDate;
+    }
+
+    public void setMarried( boolean married){
+        this.married = married;
+    }
+
+    public void setSkinColor( String skinColor){
+        this.skinColor = skinColor;
+    }
+
+
         //Get methods
     public int getAge(){
         return this.age;
     }
+
     public boolean getGenre(){
         return this.genre;
     }
+
     public String getName(){
         return this.name;
     }
 
+    public String getId(){
+        return this.id;
+    }
 
+    public String getRh(){
+        return this.rh;
+    }
 
+    public float getHeight(){
+        return this.height;
+    }
 
+    public float getWeight(){
+        return this.weight;
+    }
+
+    public Date getBornDate(){
+        return this.bornDate;
+    }
+
+    public boolean getMarried(){
+        return this.married;
+    }
+
+    public String getSkinColor(){
+        return this.skinColor;
+    }
 
 }
