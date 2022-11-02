7ef0ac6d35b78999e7826b083cdc95ea2ce035c3
NiiSV811 <sarmiento.vargasnicolas.8@gmail.com>
2022-11-02 07:39:16 -0500

    Finish Math Utils

--- javaExamplesInClass/src/co/edu/uptc/model/MathUtil.java
+++ javaExamplesInClass/src/co/edu/uptc/model/MathUtil.java
@@ -38,5 +38,111 @@ public class MathUtil {
 
     }
 
+    public static String calcCousinNumbers(int limit){
+        String primeNumbers = "";
+        int iterator = 2;
+        boolean isPrime;
+        while (iterator <=limit){
+            isPrime = true;
+            for (int count = 2; count < iterator; count++){
+                if(iterator % count == 0 ){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                primeNumbers = primeNumbers + ", " + iterator + " ";
+            }
+            iterator++;
+        }
+
+        primeNumbers = primeNumbers.replaceFirst(",", "");
+        primeNumbers = "Los numeros primos del 0 al " + limit + " son ->" + primeNumbers;
+
+        return primeNumbers;
+    }
+
+
+    public static Boolean isCapicua(String numberToCheck){
+        boolean isCapicua = false;
+        String inverseNumber = "";
+        int iterator = numberToCheck.length() - 1;
+        while(0 <= iterator){
+            inverseNumber = inverseNumber + numberToCheck.charAt(iterator);
+
+            iterator--;
+        }
+
+        if (inverseNumber.equals(numberToCheck)){
+            return true;
+        }
+        return false;
+
+    }
+
+    public static String calcEvenNumbersFromTo(int floorLimit, int topLimit){
+        String evenNums = "";
+        int iterator = floorLimit;
+        while (iterator <= topLimit ){
+
+            if (iterator%2 == 0){
+                evenNums = evenNums + ", " + iterator + " ";
+            }
+
+            iterator++;
+        }
+
+        evenNums = evenNums.replaceFirst(",", "");
+        evenNums = "Los numeros pares de "+ floorLimit +" hasta " + topLimit +" son ->"+ evenNums;
+
+        return evenNums;
+
+    }
+
+    public static String calcOddNumbersFromTo(int floorLimit, int topLimit){
+        String oddNumbers = "";
+        int iterator = floorLimit;
+
+        while (iterator <= topLimit){
+            if (iterator % 2 != 0){
+                oddNumbers = oddNumbers + ", " + iterator + " ";
+            }
+            iterator++;
+        }
+
+        oddNumbers = oddNumbers.replaceFirst(",", "");
+        oddNumbers = "Los numeros impares del"+ floorLimit +" hasta " + topLimit + " son -> " + oddNumbers;
+
+        return oddNumbers;
+    }
+
+    public static String calcCousinNumbersFromTo(int floorLimit, int topLimit){
+        String primeNumbers = "";
+        boolean isPrime;
+        int iterator = floorLimit;
+
+        while (iterator <= topLimit){
+            isPrime = true;
+            if(iterator <= 1){
+                isPrime = false;
+            }else{
+                for (int count = 2; count < iterator; count++){
+                    if(iterator % count == 0){
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime){
+                primeNumbers = primeNumbers + ", " + iterator + " ";
+            }
+            iterator++;
+        }
+
+        primeNumbers = primeNumbers.replaceFirst(",", "");
+        primeNumbers = "Los numeros primos del "+ floorLimit + " al " + topLimit + " son ->" + primeNumbers;
+
+        return primeNumbers;
+    }
 
 }
--- javaExamplesInClass/src/co/edu/uptc/view/View.java
+++ javaExamplesInClass/src/co/edu/uptc/view/View.java
@@ -2,13 +2,84 @@ package co.edu.uptc.view;
 
 import co.edu.uptc.model.MathUtil;
 
+import javax.swing.*;
+import java.text.ParseException;
+
 public class View {
     public static void main(String[] args) {
 
-        int limit = 10;
-        System.out.println(MathUtil.calcEvenNumbers(10));
+        int limit , floorLimit , topLimit = 0;
+        String numCapicuaCheck , auxLimit,auxFloor, auxTop = "";
 
-        System.out.println(MathUtil.calcOddNumbers(10));
+        auxLimit = JOptionPane.showInputDialog("Calcular Numeros pares del 0 hasta: ");
+        try {
+            limit = Integer.parseInt(auxLimit);
+            JOptionPane.showMessageDialog(null, MathUtil.calcEvenNumbers(limit));
+        }catch (Exception ParseException){
+            JOptionPane.showMessageDialog(null, "Valor incorrecto");
+        }
+
+        auxLimit = JOptionPane.showInputDialog("Calcular Numeros impares del 0 hasta: ");
+        try {
+            limit = Integer.parseInt(auxLimit);
+            JOptionPane.showMessageDialog(null, MathUtil.calcOddNumbers(limit));
+        }catch (Exception ParseException){
+            JOptionPane.showMessageDialog(null, "Valor incorrecto");
+        }
+
+        auxLimit = JOptionPane.showInputDialog("Calcular Numeros primos del 0 hasta: ");
+        try {
+            limit = Integer.parseInt(auxLimit);
+            JOptionPane.showMessageDialog(null, MathUtil.calcCousinNumbers(limit));
+        }catch (Exception ParseException){
+            JOptionPane.showMessageDialog(null, "Valor incorrecto");
+        }
+
+        numCapicuaCheck = JOptionPane.showInputDialog("Igrese numero para comprobar si es capicua: ");
+        if(MathUtil.isCapicua(numCapicuaCheck)){
+            JOptionPane.showMessageDialog(null, numCapicuaCheck + " Si es capicua");
+        }else {
+            JOptionPane.showMessageDialog(null, numCapicuaCheck + " NO es capicua");
+        }
+
+        auxFloor = JOptionPane.showInputDialog("Elija valor mínimo para calcular pares");
+        auxTop = JOptionPane.showInputDialog("Elija valor máximo para calcular pares");
+
+        try {
+            floorLimit = Integer.parseInt(auxFloor);
+            topLimit = Integer.parseInt(auxTop);
+
+            JOptionPane.showMessageDialog(null, MathUtil.calcEvenNumbersFromTo(floorLimit, topLimit));
+
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Valores incorrectos :(");
+        }
+
+        auxFloor = JOptionPane.showInputDialog("Elija valor mínimo para calcular impares");
+        auxTop = JOptionPane.showInputDialog("Elija valor máximo para calcular impares");
+
+        try {
+            floorLimit = Integer.parseInt(auxFloor);
+            topLimit = Integer.parseInt(auxTop);
+
+            JOptionPane.showMessageDialog(null, MathUtil.calcOddNumbersFromTo(floorLimit, topLimit));
+
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Valores incorrectos :(");
+        }
+
+        auxFloor = JOptionPane.showInputDialog("Elija valor mínimo para calcular primos");
+        auxTop = JOptionPane.showInputDialog("Elija valor máximo para calcular primos ");
+
+        try {
+            floorLimit = Integer.parseInt(auxFloor);
+            topLimit = Integer.parseInt(auxTop);
+
+            JOptionPane.showMessageDialog(null, MathUtil.calcCousinNumbersFromTo(floorLimit, topLimit));
+
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Valores incorrectos :(");
+        }
 
     }
 }
